de0bbce8570b9015b24c83b5e15f952aa508db59
Tensae <tensaeb2016@gmail.com>
2024-05-14 00:39:42 +0300

    form

---
+++ hibret-front-main/src/components/FormBuilderTrial.tsx
@@ -0,0 +1,286 @@
+// import { ErrorMessage } from "@hookform/error-message";
+import ReactQuill from "react-quill";
+import "quill/dist/quill.snow.css";
+import clsx from "clsx";
+import { JSXElementConstructor, Key, ReactElement, ReactNode, ReactPortal } from "react";
+import { useFormContext } from "react-hook-form";
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
+import { useEffect } from "react";
+
+export const FormBuilderTrial = ({
+  title,
+  type,
+  options,
+  index,
+//   required,
+//   parentIndex,
+}: any) => {
+  var modules = {
+    toolbar: [
+      [{ size: ["small", false, "large", "huge"] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      ["link", "image"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+        { align: [] },
+      ],
+      [
+        {
+          color: [
+            "#000000",
+            "#e60000",
+            "#ff9900",
+            "#ffff00",
+            "#008a00",
+            "#0066cc",
+            "#9933ff",
+            "#ffffff",
+            "#facccc",
+            "#ffebcc",
+            "#ffffcc",
+            "#cce8cc",
+            "#cce0f5",
+            "#ebd6ff",
+            "#bbbbbb",
+            "#f06666",
+            "#ffc266",
+            "#ffff66",
+            "#66b966",
+            "#66a3e0",
+            "#c285ff",
+            "#888888",
+            "#a10000",
+            "#b26b00",
+            "#b2b200",
+            "#006100",
+            "#0047b2",
+            "#6b24b2",
+            "#444444",
+            "#5c0000",
+            "#663d00",
+            "#666600",
+            "#003700",
+            "#002966",
+            "#3d1466",
+            "custom-color",
+          ],
+        },
+      ],
+    ],
+  };
+
+  var formats = [
+    "header",
+    "height",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "color",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "align",
+    "size",
+  ];
+//   const {
+//     register,
+//     setError,
+//     setValue,
+//     clearErrors,
+//     watch,
+//     formState: { errors },
+//   } = useFormContext();
+
+//   useEffect(() => {
+//     register(`section.${parentIndex}.content[${index}].value`, {
+//       required: true,
+//     });
+//   }, [type]);
+
+//   const editorContent = watch(`section.${parentIndex}.content[${index}].value`);
+//   const errorClassNames = ["input-error", "textarea-error"];
+
+//   const validateDocumentImage = (title: string | readonly string[] | undefined, file: File) => {
+//     // validate the size
+//     if (file.type === "application/pdf") {
+//       setValue(title, file);
+//       clearErrors(title);
+//     }
+//     if (file.type != "application/pdf") {
+//       setError(title, {
+//         type: "filetype",
+//         message: "Only PDFs are valid.",
+//       });
+
+//       return;
+//     }
+//   };
+
+//   const onEditorStateChange = (title: string, editorState: string) => {
+//     setValue(title, editorState);
+//   };
+
+  const handleInput = () => {
+    console.log(title, 'title')
+    if (type === "select") {
+      return (
+        <>
+           <Field  as="select"  name={`content.${index}.title`}  required   className="text-[#667085] w-full text-sm border border-[#EFEFF4] rounded-lg p-3 ">
+                   <option label="Select" value="" />
+                         { options?.map((option:any) => 
+                         <option key={option} label={option} value={option} />) }
+                                </Field>
+        </>
+      );
+    } else if (type === "text") {
+      return (
+        <>
+        {/* <label htmlFor={`content.${index}.${title}`}>{title}</label> */}
+           <Field
+                        type="text"
+                        id={`content.${index}.${title}`}
+                        name={`content.${index}.${title}`}
+                        className="border rounded-md p-2 mt-1 w-full"
+                        required
+                      />
+        </>
+      );
+    }
+    //  else if (type === "upload") {
+    //   return (
+    //     <>
+    //       <div
+    //         className={clsx("flex items-center justify-center w-full", {
+    //         //   [errorClassNames.join(" ")]: errors?.[title],
+    //         })}
+    //       >
+    //         <label
+    //           htmlFor="dropzone-file"
+    //           className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 "
+    //         >
+    //           <div className="flex flex-col items-center justify-center pt-5 pb-6">
+    //             <img
+    //               src="/asset/icons/upload-icon.svg"
+    //               className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+    //             />
+
+    //             <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+    //               <span className="font-semibold">Click to upload</span> or drag
+    //               and drop
+    //             </p>
+    //             <p className="text-xs text-gray-500 dark:text-gray-400">
+    //               PDF, CSV, and JPEG (MAX. 800x400px)
+    //             </p>
+    //           </div>
+    //           <input
+    //             type="file"
+    //             required={required}
+    //             // accept="image/png, image/gif, image/jpeg"
+    //             // onChange={(e) =>
+    //             //   validateDocumentImage(
+    //             //     `section.${parentIndex}.content[${index}].value`,
+    //             //     e.target.files[0]
+    //             //   )
+    //             // }
+    //           />
+    //         </label>
+    //       </div>
+    //       <span className="label-text-alt text-[hsl(var(--er))]">
+    //         <ErrorMessage
+    //         //   errors={errors}
+    //           name={`section.${parentIndex}.content[${index}].value`}
+    //         />
+    //       </span>
+    //     </>
+    //   );
+    // }
+    else if(type == 'radio'){
+        return(
+            <>
+           
+            <div
+            role="group"
+            aria-labelledby="my-radio-group "
+            className="w-full flex gap-10 "
+          >
+           {options?.map((option: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined)=>(
+            <label>
+            <Field type="radio" name={`content.${index}.${title}`}  required value={option} />
+            {option}
+          </label>
+          ))}
+          </div>
+          </>
+        )
+       
+    } else if( type== "checkbox"){
+       
+        return (
+            
+                 
+            <label>
+            <Field type="checkbox" name={`content.${index}.${title}`} />
+            {/* {title} */}
+          </label>
+
+           
+            
+          
+        )
+        
+    }
+    //     else {
+    //   // console.log(editorContent);
+    //   return (
+    //     <>
+    //       <div className="w-full">
+    //         <ReactQuill
+    //           theme="snow"
+    //           modules={modules}
+    //           formats={formats}
+    //           placeholder="write your content ...."
+    //         //   value={editorContent}
+    //         //   onChange={(editorState) =>
+    //             // onEditorStateChange(
+    //             //   `section.${parentIndex}.content[${index}].value`,
+    //             //   editorState
+    //             // )
+    //         //   }
+    //         />
+    //       </div>
+    //       <span className="label-text-alt text-[hsl(var(--er))]">
+    //         <ErrorMessage
+    //         //   errors={errors}
+    //           name={`section.${parentIndex}.content[${index}].value`}
+    //         />
+    //       </span>
+    //     </>
+    //   );
+    }
+  
+
+
+  return (
+    <div>
+      <div className="my-3">
+        <label className="label">
+          <span className="label-text">{title}</span>
+        </label>
+
+        {handleInput()}
+        {/* <HandleInput /> */}
+        {/*         
+        <button onClick={() => remove(index)}>Remove</button> */}
+      </div>
+    </div>
+  );
+};
+
--- hibret-front-main/src/routes/documenttempadd.lazy.tsx
+++ hibret-front-main/src/routes/documenttempadd.lazy.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
-import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FormikHelpers,
+  FieldArray,
+} from "formik";
+import { FormBuilderTrial } from "../components/FormBuilderTrial";
 import "react-dropdown/style.css";
-import { FormControlLabel, Radio, RadioGroup } from "@mui/material";
 
 export const Route = createFileRoute("/documenttempadd")({
   component: () => <DocumentAddTemp onClose={undefined} />,
@@ -12,97 +19,155 @@ const addSection = [
     sectionId: 1,
     title: "Sections",
     content: [
+      
       { title: "Section-Header", type: "text" },
       {
         title: "Section-Type",
         type: "select",
-        options: ["Short-Text", "Long-Text", "Select-options", "Uploaded-Document"],
-        
+        options: [
+          "Short-Text",
+          "Long-Text",
+          "Select-options",
+          "Uploaded-Document",
+        ],
       },
-      { title: "Section-Necessity", type: "radio", options: ["Required", "Optional"] },
-      { title: "Eligible-as-condition-criteria", type: "Checkbox" },
-
+      {
+        title: "Section-Necessity",
+        type: "radio",
+        options: ["Required", "Optional"],
+      },
+      {
+        title: "Eligible-as-condition-criteria",
+        type: "checkbox",
+      },
     ],
-    multiple: false,
   },
-]
+];
 function DocumentAddTemp({ onClose }) {
   return (
     <div className="w-full h-fit max-w-[865px] absolute top-32 right-72 bg-white z-20 px-10 py-4 border-2 rounded-lg">
       <Formik
         initialValues={{
-          documentName: "",
-          documentCategory: "",
-          documentSubcategory: "",
-          documentType: "",
-          sectionHeader: "",
-          eligible: "",
-          required: "",
+          content: addSection[0].content
         }}
-        onSubmit={(values: any, { setSubmitting }: FormikHelpers<any>) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 500);
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
         }}
       >
-        <div className="">
-          <div className="flex justify-between items-center mb-4">
-            <button
-              onClick={onClose}
-              className="flex items-center text-gray-600 hover:text-gray-800 focus:outline-none"
-            >
-              <img src="/asset/icons/back-arrow.svg" className="w-8 h-8 mr-2" />{" "}
-              {/* Adjust width and height */}
-              <h2 className="text-[#4A176D] text-3xl font-bold">
-                Add New Document Template
-              </h2>
-            </button>
-          </div>
+        {({ values }) => (
+          <div className="">
+            <div className="flex justify-between items-center mb-4">
+              <button
+                onClick={onClose}
+                className="flex items-center text-gray-600 hover:text-gray-800 focus:outline-none"
+              >
+                <img
+                  src="/asset/icons/back-arrow.svg"
+                  className="w-8 h-8 mr-2"
+                />{" "}
+                {/* Adjust width and height */}
+                <h2 className="text-[#4A176D] text-3xl font-bold">
+                  Add New Document Template
+                </h2>
+              </button>
+            </div>
 
-          <Form>
-            <div className="mb-6 rounded-lg overflow-hidden flex flex-col gap-10">
-              {/* section1 */}
-              <div className="flex flex-col gap-5 p-6 border border-[#EFEFF4] rounded-lg">
-                <h3 className="text-[#00B0AD] text-1xl font-bold">
-                  Document Information
-                </h3>
-                {/* Document Name */}
-                <div className="mt-4">
-                  <label htmlFor="documentName" className="text-sm w-full">
-                    Document Name*
-                  </label>
-                  <Field
-                    type="text"
-                    name="documentName"
-                    className="border rounded-md p-2 mt-1 w-full" // Set width to full and remove fixed width
-                    required
-                  />
-                </div>
-                {/* Document Type */}
-                <div className="flex w-full gap-3">
-                  <div className="w-full flex flex-col justify-center gap-2">
-                    <label className="text-sm w-full">Document Category*</label>
-                    <Field
-                      name="documentCategory"
-                      type="text"
-                      className="border rounded-md p-2 mt-1 w-full"
-                    />
-                  </div>
-                  <div className="w-full flex flex-col justify-center gap-2">
-                    <label className="text-sm w-full">
-                      Document Subcategory*
+            <Form>
+              <div className="mb-6 rounded-lg overflow-hidden flex flex-col gap-10">
+                {/* section1 */}
+                <div className="flex flex-col gap-5 p-6 border border-[#EFEFF4] rounded-lg">
+                  <h3 className="text-[#00B0AD] text-1xl font-bold">
+                    Document Information
+                  </h3>
+                  {/* Document Name */}
+                  <div className="mt-4">
+                    <label htmlFor="documentName" className="text-sm w-full">
+                      Document Name*
                     </label>
                     <Field
-                      name="documentSubcategory"
                       type="text"
-                      className="border rounded-md p-2 mt-1 w-full"
+                      name="documentName"
+                      className="border rounded-md p-2 mt-1 w-full" // Set width to full and remove fixed width
+                      required
                     />
                   </div>
+                  {/* Document Type */}
+                  <div className="flex w-full gap-3">
+                    <div className="w-full flex flex-col justify-center gap-2">
+                      <label className="text-sm w-full">
+                        Document Category*
+                      </label>
+                      <Field
+                        name="documentCategory"
+                        type="text"
+                        className="border rounded-md p-2 mt-1 w-full"
+                      />
+                    </div>
+                    <div className="w-full flex flex-col justify-center gap-2">
+                      <label className="text-sm w-full">
+                        Document Subcategory*
+                      </label>
+                      <Field
+                        name="documentSubcategory"
+                        type="text"
+                        className="border rounded-md p-2 mt-1 w-full"
+                      />
+                    </div>
+                  </div>
                 </div>
-              </div>
-              {/* section2 */}
-              <div className="flex flex-col gap-5 p-6 border border-[#EFEFF4] rounded-sm ">
+                {/* section2 */}
+
+                {addSection.map((item) => (
+                  
+                  <div className="flex flex-col gap-5 p-6 border border-[#EFEFF4] rounded-sm ">
+                    <h3 className="text-[#00B0AD] text-1xl font-bold">
+                      {item.title}
+                    </h3>
+                    <div className="flex flex-col w-full gap-3">
+                      <FieldArray name="content">
+                        {({ push }) => (
+                          <div>
+                            {values.content.length > 0 &&
+                            values.content.map((contents, idx) => {
+                              {
+                                console.log({ addSection }, "here");
+                              }
+                              return (
+                                <div key={idx}>
+                                  <FormBuilderTrial
+                                    // title={contents.title}
+                                    // type={contents.type}
+                                    // options={contents.options}
+                                    {...contents}
+                                    index={idx}
+                                    // parentIndex={parentIndex}
+                                  />
+                                </div>
+                              );
+                            })}
+                            <button
+                              type="button"
+                              onClick={() => {
+                                try {
+                                  push({ 
+                                    ...addSection[0].content
+                                  });
+                                  console.log("Section added");
+                                } catch (error) {
+                                  console.error("Error occurred while adding section:", error);
+                                }
+                              }}
+                            >
+                              add
+                            </button>
+                          </div>
+                        )}
+                      </FieldArray>
+                    </div>
+                  </div>
+                ))}
+                {/* <div className="flex flex-col gap-5 p-6 border border-[#EFEFF4] rounded-sm ">
                 <h3 className="text-[#00B0AD] text-1xl font-bold">Sections</h3>
                 <div className="flex flex-col w-full gap-3">
                   <div className="w-full flex flex-col justify-center gap-2">
@@ -162,25 +227,26 @@ function DocumentAddTemp({ onClose }) {
                     </div>
                   </div>
                 </div>
+              </div> */}
               </div>
-            </div>
 
-            <div className="flex items-center justify-center text-center w-full text-[#00B0AD] text-base border border-[#00B0AD] rounded-lg p-2">
-              {" "}
-              <img src="/asset/icons/plus-black.svg" />
-              Add More Sections
-            </div>
-            {/* Submit Button */}
-            <div className="flex gap-4 justify-end items-center mt-6">
-              <button
-                type="submit"
-                className="bg-gray-300 hover:bg-gray-400 text-gray-800 text-base py-2 px-4 rounded"
-              >
-                Publish Template
-              </button>
-            </div>
-          </Form>
-        </div>
+              <div className="flex items-center justify-center text-center w-full text-[#00B0AD] text-base border border-[#00B0AD] rounded-lg p-2">
+                {" "}
+                <img src="/asset/icons/plus-black.svg" />
+                Add More Sections
+              </div>
+              {/* Submit Button */}
+              <div className="flex gap-4 justify-end items-center mt-6">
+                <button
+                  type="submit"
+                  className="bg-gray-300 hover:bg-gray-400 text-gray-800 text-base py-2 px-4 rounded"
+                >
+                  Publish Template
+                </button>
+              </div>
+            </Form>
+          </div>
+        )}
       </Formik>
     </div>
   );
