6280f0b0e7d71230fd7b7d3bd7eab5c2f2525588
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-05-15 21:07:16 +0300

    Update userDashboard.lazy.tsx

--- hibret-front-main/src/routes/userDashboard.lazy.tsx
+++ hibret-front-main/src/routes/userDashboard.lazy.tsx
@@ -2,15 +2,15 @@ import { createFileRoute } from '@tanstack/react-router';
 import AdminHead  from '../components/adminHead';
 import UserCards from '../components/UserCards';
 import UserEnd from '../components/UserEnd';
-import NavBar from '../components/NavBar';
-import SideBar from '../components/SideBar';
+import NavBar2 from '../components/NavBar2';
+import SideBar2 from '../components/SideBar2';
 
 export const Route = createFileRoute('/userDashboard')({
   component: () => (
 
     <div>
-      <NavBar/>
-    <SideBar/>
+      <NavBar2/>
+    <SideBar2/>
     <div className="mt-24 ml-80 mr-8">
       
       <AdminHead/>
