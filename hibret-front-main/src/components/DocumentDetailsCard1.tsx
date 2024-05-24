662b72caab254c6f037ceb36c0392a57242902a3
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-05-24 13:20:43 +0300

    Update DocumentDetailsCard1.tsx

--- hibret-front-main/src/components/DocumentDetailsCard1.tsx
+++ hibret-front-main/src/components/DocumentDetailsCard1.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import download from '/asset/icons/download.svg';
 import pdf from '/asset/icons/pdf.svg';
 import visible from '/asset/icons/visible.svg';
+import { Link } from '@tanstack/react-router';
 
 interface DocumentDetailsCardProps {
   name: string;
+  link:string
 }
 
 const DocumentDetailsCard: React.FC<DocumentDetailsCardProps> = (props) => {
   return (
     <div className="flex justify-between border-b pb-4"> 
-      <div className="flex gap-5 items-center">
+      <Link to={props.link} className="flex gap-5 items-center">
         <img src={pdf} className='h-4' alt="PDF Icon"/>
         <h5 className="text-teal-600 text-sm">{props.name}</h5>
-      </div>
+      </Link>
       <div className="flex gap-6 items-center">
         <img src={visible} className='h-4 w-4' alt="Visible Icon"/>
         <img src={download} className='h-4 w-4' alt="Download Icon"/>
