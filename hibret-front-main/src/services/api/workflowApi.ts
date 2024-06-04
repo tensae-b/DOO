43dd57020865faadc39b382695df7a0c2aa1da88
Tensae <tensaeb2016@gmail.com>
2024-06-05 00:13:52 +0300

    merge assigned by me and document

--- hibret-front-main/src/services/api/workflowApi.ts
+++ hibret-front-main/src/services/api/workflowApi.ts
@@ -13,5 +13,13 @@ export const getRequiredDocument = async (workflowId: any) => {
 };
 
 
-
-
+export const fetchWorkflowName = async () => {
+    try {
+        const response = await axiosInst.get(`http://localhost:5000/admin/workflow-templates`)
+        const { data } = response;
+        return { data, isLoading: false, isError: false }; // assuming isLoading and isError are handled elsewhere
+    } catch (error) {
+        console.error('Error fetching catagory:', error);
+        return { data: null, isLoading: false, isError: true };
+    }
+};
