40a2c7d33e0fb4cce2991e3bb2e97fd9ff32d781
tensae-b <tensaeb2016@gmail.com>
2024-06-06 15:47:23 +0300

    homepage

--- hibret-front-main/src/routes/index.lazy.tsx
+++ hibret-front-main/src/routes/index.lazy.tsx
@@ -1,8 +1,35 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
 
-export const Route = createLazyFileRoute('/')({
-    component: () => <div>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1></div>
-})
+export const Route = createLazyFileRoute("/")({
+  component: () => <Homepage />,
+});
+
+function Homepage() {
+  const navigate = useNavigate();
+  const [login, setLogin] = useState(false);
+  const [transition, setTransition] = useState("");
+  useEffect(() => {
+    setTimeout(() => {
+      setLogin(true);
+    }, 500);
+
+    if (login) {
+      setTransition("animate-ping");
+
+      setTimeout(() => {
+        navigate({ to: "/login" });
+      }, 900);
+    }
+  }, [login]);
+  return (
+    <div
+      className={`w-screen h-screen ${transition} flex flex-col justify-center items-center`}
+    >
+      <img className=" w-96 h-96 " src="/asset/hibret-logo.svg" />
+      <h1 className="text-3xl text-[#4A176D] -mt-20">
+        Document Workflow Management and Approval System
+      </h1>
+    </div>
+  );
+}
